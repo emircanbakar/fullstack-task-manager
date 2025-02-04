c01cabce0a5b1e8b471f99cdf432703494292fc9
emircanbakar <emirbakar952@gmail.com>
2025-02-04 09:58:25 +0300

    login

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,4 @@
-const Auth = require("../models/Auth");
+const User = require("../models/Auth");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
@@ -15,11 +15,9 @@ const register = async () => {
 
     const existingUser = await User.findOne({ email });
     if (existingUser) {
-      return res
-        .status(400)
-        .json({
-          message: "Bu e-posta ile bir hesap daha önceden oluşturulmuş.",
-        });
+      return res.status(400).json({
+        message: "Bu e-posta ile bir hesap daha önceden oluşturulmuş.",
+      });
     }
 
     const passwordHash = await bcrypt.hash(password, 10);
@@ -27,6 +25,7 @@ const register = async () => {
     await newUser.save();
 
     res.status(201).json({ message: "Kayıt Başarılı!" });
+    console.log(token)
   } catch (error) {
     res
       .status(500)
@@ -34,3 +33,35 @@ const register = async () => {
     console.error(error, "error");
   }
 };
+
+const login = async () => {
+  try {
+    const { username, password } = req.body;
+    if (!username || !password) {
+      res
+        .status(400)
+        .json({ message: "Lütfen kullanıcı adı ve şifre bilginizi giriniz!" });
+    }
+
+    const user = await User.findOne({ username });
+    if (!user) res.status(400).json({ message: "Böyle bir kullanıcı yok!" });
+
+    const isUserMatch = await bcrypt.compare(password, user.password);
+    if (!isUserMatch)
+      res.status(400).json({ message: "Giriş Bilgileri Hatalı!" });
+
+    const token = jwt.sign({ id: user._id }, JWT_SECRET, { expiresIn: "1h" });
+
+    res
+      .cookie("token", token, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+      })
+      .status(200)
+      .json({ message: "Giriş Başarılı!", token });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+module.exports = { login, register };
